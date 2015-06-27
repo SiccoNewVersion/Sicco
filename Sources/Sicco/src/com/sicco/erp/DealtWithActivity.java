4e6bb9338b5de33f9b69a8a5048515c5dd2e5c21
luandt1005 <luandt1005@gmail.com>
2015-06-27 11:19:09 +0700

    LuanDT add code

--- Sources/Sicco/src/com/sicco/erp/DealtWithActivity.java
+++ Sources/Sicco/src/com/sicco/erp/DealtWithActivity.java
@@ -1,14 +1,192 @@
 package com.sicco.erp;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Window;
+import java.util.ArrayList;
 
-public class DealtWithActivity extends Activity{
+import com.sicco.erp.adapter.TaskAdapter;
+import com.sicco.erp.model.Dispatch;
+import com.sicco.erp.model.Dispatch.OnLoadListener;
+import com.sicco.erp.util.Keyboard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class DealtWithActivity extends Activity implements OnClickListener, OnItemClickListener{
+	
+	private LinearLayout searchView, connectError;
+	private ImageView back, search, close, empty;
+	private EditText editSearch;
+	private ListView listDispatch;
+	private ProgressBar loading;
+	private Button retry;
+	private TaskAdapter adapter;
+	private ArrayList<Dispatch> arrDispatch;
+	private Dispatch dispatch;
+	private TextView title_actionbar;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_ACTION_BAR);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_approval);
+		init();
+	}
+
+	private void init() {
+		searchView = (LinearLayout) findViewById(R.id.searchview);
+		back = (ImageView) findViewById(R.id.back);
+		search = (ImageView) findViewById(R.id.search);
+		close = (ImageView) searchView.findViewById(R.id.close);
+		empty = (ImageView) searchView.findViewById(R.id.empty);
+		editSearch = (EditText) searchView.findViewById(R.id.edit_search);
+		listDispatch = (ListView) findViewById(R.id.listDispatch);
+		loading = (ProgressBar) findViewById(R.id.loading);
+		retry = (Button) findViewById(R.id.retry);
+		connectError = (LinearLayout) findViewById(R.id.connect_error);
+		title_actionbar = (TextView)  findViewById(R.id.title_actionbar);
+		title_actionbar.setText(getResources().getString(R.string.cv_xu_ly));
+		// click
+		back.setOnClickListener(this);
+		search.setOnClickListener(this);
+		close.setOnClickListener(this);
+		empty.setOnClickListener(this);
+		retry.setOnClickListener(this);
+		listDispatch.setOnItemClickListener(this);
+		// set adapter
+		dispatch = new Dispatch(DealtWithActivity.this);
+		arrDispatch = dispatch.getData("http://myapp.freezoy.com/",
+				new OnLoadListener() {
+
+					@Override
+					public void onStart() {
+						loading.setVisibility(View.VISIBLE);
+						connectError.setVisibility(View.GONE);
+					}
+
+					@Override
+					public void onSuccess() {
+						loading.setVisibility(View.GONE);
+						adapter.notifyDataSetChanged();
+					}
+
+					@Override
+					public void onFalse() {
+						loading.setVisibility(View.GONE);
+						connectError.setVisibility(View.VISIBLE);
+					}
+				});
+		adapter = new TaskAdapter(DealtWithActivity.this,
+				arrDispatch);
+		listDispatch.setAdapter(adapter);
+	}
+
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+		switch (id) {
+		case R.id.back:
+			finish();
+			break;
+		case R.id.search:
+			showSearchView();
+			break;
+		case R.id.close:
+			closeSearchView();
+			break;
+		case R.id.empty:
+			editSearch.setText("");
+			break;
+		case R.id.retry:
+			adapter.setData(dispatch.getData(
+					"http://myapp.freezoy.com/", new OnLoadListener() {
+
+						@Override
+						public void onStart() {
+							loading.setVisibility(View.VISIBLE);
+							connectError.setVisibility(View.GONE);
+						}
+
+						@Override
+						public void onSuccess() {
+							loading.setVisibility(View.GONE);
+							// listDispatch.setAdapter(dispatchAdapter);
+							adapter.notifyDataSetChanged();
+						}
+
+						@Override
+						public void onFalse() {
+							loading.setVisibility(View.GONE);
+							connectError.setVisibility(View.VISIBLE);
+						}
+					}));
+			break;
+		}
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		Intent intent = new Intent(DealtWithActivity.this,
+				ViewDispatchActivity.class);
+		startActivity(intent);
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (searchView.getVisibility() == View.VISIBLE) {
+			searchView.setVisibility(View.GONE);
+		} else {
+			super.onBackPressed();
+		}
+	}
+
+	private void showSearchView() {
+		searchView.setVisibility(View.VISIBLE);
+		searchView.requestFocus();
+		Keyboard.showKeyboard(DealtWithActivity.this, editSearch);
+		editSearch.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				if (arg0.toString().trim().length() > 0) {
+					empty.setVisibility(View.VISIBLE);
+				} else {
+					empty.setVisibility(View.GONE);
+				}
+				ArrayList<Dispatch> searchData = dispatch.search(arg0
+						.toString().trim());
+				adapter.setData(searchData);
+				adapter.notifyDataSetChanged();
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
+			}
+
+			@Override
+			public void afterTextChanged(Editable arg0) {
+			}
+		});
+	}
+
+	private void closeSearchView() {
+		Keyboard.hideKeyboard(DealtWithActivity.this, editSearch);
+		searchView.setVisibility(View.GONE);
+		editSearch.setText("");
 	}
 }
---
+++ Sources/Sicco/src/com/sicco/erp/adapter/TaskAdapter.java
@@ -0,0 +1,110 @@
+package com.sicco.erp.adapter;
+
+import java.util.ArrayList;
+
+import com.sicco.erp.R;
+import com.sicco.erp.model.Dispatch;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.BaseAdapter;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TaskAdapter extends BaseAdapter {
+	private Context context;
+	private ArrayList<Dispatch> data;
+
+	public TaskAdapter(Context context, ArrayList<Dispatch> data) {
+		this.context = context;
+		this.data = data;
+	}
+
+	public void setData(ArrayList<Dispatch> data) {
+		this.data = data;
+	}
+
+	@Override
+	public int getCount() {
+		return data.size();
+	}
+
+	@Override
+	public Dispatch getItem(int arg0) {
+		return data.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		return data.get(arg0).getId();
+	}
+
+	@Override
+	public View getView(int position, View view, ViewGroup viewGroup) {
+		final ViewHolder holder;
+		Dispatch dispatch = getItem(position);
+		if (view == null) {
+			view = LayoutInflater.from(context).inflate(R.layout.item_dispatch,
+					viewGroup, false);
+			holder = new ViewHolder();
+			holder.title = (TextView) view.findViewById(R.id.title);
+			holder.description = (TextView) view.findViewById(R.id.description);
+			holder.approval = (TextView) view.findViewById(R.id.approval);
+			holder.approval.setText(context.getResources().getString(R.string.task));
+			view.setTag(holder);
+		} else {
+			holder = (ViewHolder) view.getTag();
+		}
+		holder.title.setText(dispatch.getTitle());
+		holder.description.setText(dispatch.getDescription());
+		holder.approval.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				PopupMenu popupMenu = new PopupMenu(context, holder.approval);
+				popupMenu.getMenuInflater().inflate(R.menu.menu_task, popupMenu.getMenu());
+				popupMenu.show();
+				popupMenu.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+					
+					@Override
+					public boolean onMenuItemClick(MenuItem item) {
+						switch (item.getItemId()) {
+						case R.id.action_handle:
+							Toast.makeText(context, item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+							break;
+						case R.id.action_steer:
+							Toast.makeText(context, item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+							break;
+						case R.id.action_report:
+							Toast.makeText(context, item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+							break;
+						case R.id.action_change_status:
+							Toast.makeText(context, item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+							break;
+						case R.id.action_job_transfer:
+							Toast.makeText(context, item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+							break;
+
+						default:
+							break;
+						}
+						return false;
+					}
+				});
+			}
+		});
+		return view;
+	}
+
+	private class ViewHolder {
+		TextView title;
+		TextView description;
+		TextView approval;
+	}
+}
