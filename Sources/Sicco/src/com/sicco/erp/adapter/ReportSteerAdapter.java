04e990261b3674a7712e1bc854bac65fd59723b1
luandt1005 <luandt1005@gmail.com>
2015-06-30 15:56:00 +0700

    LuanDT edit view

--- Sources/Sicco/src/com/sicco/erp/adapter/ReportSteerAdapter.java
+++ Sources/Sicco/src/com/sicco/erp/adapter/ReportSteerAdapter.java
@@ -6,6 +6,7 @@ import com.sicco.erp.R;
 import com.sicco.erp.model.ReportSteer;
 
 import android.content.Context;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,11 +17,12 @@ public class ReportSteerAdapter extends BaseAdapter {
 
 	private Context context;
 	private ArrayList<ReportSteer> data;
-//	private int intResource;
+
+	// private int intResource;
 	public ReportSteerAdapter(Context context, ArrayList<ReportSteer> data) {
 		this.context = context;
 		this.data = data;
-//		this.intResource = resource;
+		// this.intResource = resource;
 	}
 
 	public Context getContext() {
@@ -44,8 +46,6 @@ public class ReportSteerAdapter extends BaseAdapter {
 		return data.size();
 	}
 
-	
-
 	@Override
 	public ReportSteer getItem(int position) {
 		return data.get(position);
@@ -61,25 +61,43 @@ public class ReportSteerAdapter extends BaseAdapter {
 		ViewHolder viewHolder;
 		ReportSteer reportSteer = getItem(position);
 		if (convertView == null) {
-			convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_report_steer, parent,false);
+			convertView = LayoutInflater.from(getContext()).inflate(
+					R.layout.item_report_steer, parent, false);
 			viewHolder = new ViewHolder();
-			viewHolder.handler = (TextView)convertView.findViewById(R.id.txtHandler);
-			viewHolder.date = (TextView)convertView.findViewById(R.id.txtDate);
-			viewHolder.contentReport = (TextView)convertView.findViewById(R.id.txtContentReportSteer);
-			
+			viewHolder.handler = (TextView) convertView
+					.findViewById(R.id.txtHandler);
+			viewHolder.date = (TextView) convertView.findViewById(R.id.txtDate);
+			viewHolder.contentReport = (TextView) convertView
+					.findViewById(R.id.txtContentReportSteer);
+
 			convertView.setTag(viewHolder);
-		}else {
+		} else {
 			viewHolder = (ViewHolder) convertView.getTag();
 		}
-		
-		viewHolder.handler.setText(reportSteer.getHandler());
-		viewHolder.date.setText(reportSteer.getDate());
-		viewHolder.contentReport.setText(reportSteer.getContent());
-		
+		if (position % 2 == 0) {
+			convertView.setBackgroundColor(context.getResources().getColor(
+					R.color.item_color));
+		}else{
+			convertView.setBackgroundColor(context.getResources().getColor(
+					R.color.white));
+		}
+		String handler = "<font weigth='bold'><b><u><i>"
+				+ context.getResources().getString(R.string.handler)
+				+ "</i></u></b></font>" + "  " + reportSteer.getHandler();
+		String date = "<font weigth='bold'><b><u><i>"
+				+ context.getResources().getString(R.string.time)
+				+ "</i></u></b></font>" + "  " + reportSteer.getDate();
+		String contentReport = "<font weigth='bold'><b><u><i>"
+				+ context.getResources().getString(R.string.report)
+				+ "</i></u></b></font>" + "  " + reportSteer.getContent();
+		viewHolder.handler.setText(Html.fromHtml(handler));
+		viewHolder.date.setText(Html.fromHtml(date));
+		viewHolder.contentReport.setText(Html.fromHtml(contentReport));
+
 		return convertView;
 	}
-	
-	private class ViewHolder{
-		TextView handler,date,contentReport;
+
+	private class ViewHolder {
+		TextView handler, date, contentReport;
 	}
 }
