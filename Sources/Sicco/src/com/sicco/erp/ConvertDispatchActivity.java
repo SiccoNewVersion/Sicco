516db72e7a7650c5d7be1a2b5d03b09f590cfc73
luandt1005 <luandt1005@gmail.com>
2015-07-07 14:42:31 +0700

    LuanDT

--- Sources/Sicco/src/com/sicco/erp/ConvertDispatchActivity.java
+++ Sources/Sicco/src/com/sicco/erp/ConvertDispatchActivity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,13 +26,15 @@ import com.sicco.erp.util.DialogChoseUser;
 public class ConvertDispatchActivity extends Activity implements
 		OnClickListener {
 	private ImageView back;
-	private LinearLayout lnJobType,lnFromDate,lnStatus,lnProgress,lnLevel,lnHandler,lnViewer,lnDepartment,lnToDate;
-	private TextView txtNumSignDispatch,txtJobType,txtFromDate,txtStatus,txtProgress,txtLevel,txtDepartment,txtToDate;
+	private LinearLayout lnJobType, lnFromDate, lnStatus, lnProgress, lnLevel,
+			lnHandler, lnViewer, lnDepartment, lnToDate;
+	private TextView txtNumSignDispatch, txtJobType, txtFromDate, txtStatus,
+			txtProgress, txtLevel, txtDepartment, txtToDate;
 	private EditText edtTitleJob;
 	private Button btnConvert;
-	
-	public static TextView txtHandler,txtViewer;
-	
+
+	public static TextView txtHandler, txtViewer;
+
 	private ArrayList<Department> listDep;
 	private ArrayList<User> allUser;
 	private ArrayList<User> listChecked;
@@ -42,42 +45,42 @@ public class ConvertDispatchActivity extends Activity implements
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_ACTION_BAR);
 		setContentView(R.layout.activity_convert_dispatch);
-		
+
 		Intent intent = getIntent();
 		dispatch = (Dispatch) intent.getSerializableExtra("dispatch");
-		
+
 		init();
 	}
 
 	private void init() {
 		back = (ImageView) findViewById(R.id.back);
-		lnJobType = (LinearLayout)findViewById(R.id.lnJobType);
-		lnFromDate = (LinearLayout)findViewById(R.id.lnFromDate);
-		lnStatus = (LinearLayout)findViewById(R.id.lnStatus);
-		lnProgress = (LinearLayout)findViewById(R.id.lnProgress);
-		lnLevel = (LinearLayout)findViewById(R.id.lnLevel);
-		lnHandler = (LinearLayout)findViewById(R.id.lnHandler);
-		lnViewer = (LinearLayout)findViewById(R.id.lnViewer);
-		lnDepartment = (LinearLayout)findViewById(R.id.lnDepartment);
-		lnToDate = (LinearLayout)findViewById(R.id.lnToDate);
-		
-		txtJobType = (TextView)findViewById(R.id.txtJobType);
-		txtFromDate = (TextView)findViewById(R.id.txtFromDate);
-		txtStatus = (TextView)findViewById(R.id.txtStatus);
-		txtProgress = (TextView)findViewById(R.id.txtProgress);
-		txtLevel = (TextView)findViewById(R.id.txtLevel);
-		txtHandler = (TextView)findViewById(R.id.txtHandler);
-		txtViewer = (TextView)findViewById(R.id.txtViewer);
-		txtDepartment = (TextView)findViewById(R.id.txtDepartment);
-		txtToDate = (TextView)findViewById(R.id.txtToDate);
-		
+		lnJobType = (LinearLayout) findViewById(R.id.lnJobType);
+		lnFromDate = (LinearLayout) findViewById(R.id.lnFromDate);
+		lnStatus = (LinearLayout) findViewById(R.id.lnStatus);
+		lnProgress = (LinearLayout) findViewById(R.id.lnProgress);
+		lnLevel = (LinearLayout) findViewById(R.id.lnLevel);
+		lnHandler = (LinearLayout) findViewById(R.id.lnHandler);
+		lnViewer = (LinearLayout) findViewById(R.id.lnViewer);
+		lnDepartment = (LinearLayout) findViewById(R.id.lnDepartment);
+		lnToDate = (LinearLayout) findViewById(R.id.lnToDate);
+
+		txtJobType = (TextView) findViewById(R.id.txtJobType);
+		txtFromDate = (TextView) findViewById(R.id.txtFromDate);
+		txtStatus = (TextView) findViewById(R.id.txtStatus);
+		txtProgress = (TextView) findViewById(R.id.txtProgress);
+		txtLevel = (TextView) findViewById(R.id.txtLevel);
+		txtHandler = (TextView) findViewById(R.id.txtHandler);
+		txtViewer = (TextView) findViewById(R.id.txtViewer);
+		txtDepartment = (TextView) findViewById(R.id.txtDepartment);
+		txtToDate = (TextView) findViewById(R.id.txtToDate);
+
 		btnConvert = (Button) findViewById(R.id.btnConvert);
-		
-		edtTitleJob = (EditText)findViewById(R.id.edtTitle);
-		
+
+		edtTitleJob = (EditText) findViewById(R.id.edtTitle);
+
 		// click
 		back.setOnClickListener(this);
-		
+
 		lnJobType.setOnClickListener(this);
 		lnFromDate.setOnClickListener(this);
 		lnStatus.setOnClickListener(this);
@@ -88,18 +91,18 @@ public class ConvertDispatchActivity extends Activity implements
 		lnDepartment.setOnClickListener(this);
 		lnToDate.setOnClickListener(this);
 		btnConvert.setOnClickListener(this);
-		
+
 		listChecked = new ArrayList<User>();
 
 		listDep = HomeActivity.listDep;
 		allUser = HomeActivity.allUser;
-		
-		//set data
-		edtTitleJob.setText(getResources().getString(R.string.action) + dispatch.getNumberDispatch());
+
+		// set data
+		edtTitleJob.setText(getResources().getString(R.string.action)
+				+ dispatch.getNumberDispatch());
 		txtFromDate.setText(dispatch.getDate());
 		txtToDate.setText(dispatch.getDate());
-		
-		
+
 	}
 
 	@Override
@@ -110,19 +113,29 @@ public class ConvertDispatchActivity extends Activity implements
 			finish();
 			break;
 		case R.id.lnJobType:
-			Toast.makeText(getApplicationContext(), getResources().getString(R.string.default_value), Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(),
+					getResources().getString(R.string.default_value),
+					Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.lnFromDate:
-			Toast.makeText(getApplicationContext(), getResources().getString(R.string.default_value), Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(),
+					getResources().getString(R.string.default_value),
+					Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.lnStatus:
-			Toast.makeText(getApplicationContext(), getResources().getString(R.string.default_value), Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(),
+					getResources().getString(R.string.default_value),
+					Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.lnProgress:
-			Toast.makeText(getApplicationContext(), getResources().getString(R.string.default_value), Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(),
+					getResources().getString(R.string.default_value),
+					Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.lnLevel:
-			Toast.makeText(getApplicationContext(), getResources().getString(R.string.default_value), Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(),
+					getResources().getString(R.string.default_value),
+					Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.lnHandler:
 
@@ -136,20 +149,50 @@ public class ConvertDispatchActivity extends Activity implements
 			TaskAdapter.flag = "chooseViewer";
 			new DialogChoseUser(ConvertDispatchActivity.this, listDep, allUser,
 					listChecked);
-			
+
 			break;
 		case R.id.lnDepartment:
-			Toast.makeText(getApplicationContext(), getResources().getString(R.string.default_value), Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(),
+					getResources().getString(R.string.default_value),
+					Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.lnToDate:
-			Toast.makeText(getApplicationContext(), getResources().getString(R.string.default_value), Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(),
+					getResources().getString(R.string.default_value),
+					Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.btnConvert:
 			Log.d("LuanDT", "userH: " + DialogChoseUser.strUsersHandl);
 			Log.d("LuanDT", "userV: " + DialogChoseUser.strUsersView);
 			Log.d("LuanDT", "id--userH: " + DialogChoseUser.idUsersHandl);
 			Log.d("LuanDT", "id--userV: " + DialogChoseUser.idUsersView);
-			
+
+			boolean error = false;
+			if (edtTitleJob.getText().toString().trim().equals("")) {
+				edtTitleJob.setError(getResources().getString(
+						R.string.truong_nay_khong_duoc_de_rong));
+				error = true;
+			} else if (txtHandler.getText().toString()
+					.equals(getResources().getString(R.string.handler1))) {
+				txtHandler.setError(getResources().getString(
+						R.string.truong_nay_khong_duoc_de_rong));
+				error = true;
+			} else if (txtViewer.getText().toString()
+					.equals(getResources().getString(R.string.viewer))) {
+				txtViewer.setError(getResources().getString(
+						R.string.truong_nay_khong_duoc_de_rong));
+				error = true;
+			} else if (txtDepartment.getText().toString()
+					.equals(getResources().getString(R.string.department))) {
+				txtDepartment.setError(getResources().getString(
+						R.string.truong_nay_khong_duoc_de_rong));
+				error = true;
+			}
+
+			if (error == false) {
+
+			}
+
 			break;
 		}
 	}
